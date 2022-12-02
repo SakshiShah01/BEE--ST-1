ae6b833c392ca07b34ddea4552fa72780e96e21f
SakshiShah01 <sakshi2075.be20@chitkara.edu.in>
2022-12-02 21:12:12 +0530

    first

---
+++ src/components/About.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './About.css'
+
+const About = () => {
+    return (
+        <div className="section">
+        <div className="container">
+            <div className="content-section">
+                <div className="title">
+                    <h1>About Us</h1><br/>
+                   
+                    <p>Book Kook is the world’s largest site for readers and book recommendations. Our mission is to help people find and share books they love. Goodreads launched in January 2069.</p>
+                </div>
+                <br/>
+                
+            </div>
+        </div>
+    </div>
+    )
+  }
+  
+  export default About
---
+++ src/components/BData.js
@@ -0,0 +1,54 @@
+const data = [
+    
+    {   id : 1,
+        title  : "Six Of Crows",
+        content : "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1651710803l/23437156.jpg",
+        about : "Ketterdam: a bustling hub of international trade where anything can be had for the right price—and no one knows that better than criminal prodigy Kaz Brekker. Kaz is offered a chance at a deadly heist that could make him rich beyond his wildest dreams. But he can’t pull it off alone. . . .",
+
+    },
+    {   id : 2,
+        title  : "The Hunger Games",
+        content : "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1586722975l/2767052._SY475_.jpg",
+        about  : "In the ruins of a place once known as North America lies the nation of Panem, a shining Capitol surrounded by twelve outlying districts. The Capitol is harsh and cruel and keeps the districts in line by forcing them all to send one boy and one girl between the ages of twelve and eighteen to participate in the annual Hunger Games, a fight to the death on live TV.     Sixteen-year-old Katniss Everdeen, who lives alone with her mother and younger sister, regards it as a death sentence when she steps forward to take her sister's place in the Games. But Katniss has been close to dead before—and survival, for her, is second nature. Without really meaning to, she becomes a contender. But if she is to win, she will have to start making choices that weight survival against humanity."
+
+    },
+    {   id : 3,
+        title  : "The Fault In Our Stars",
+        content : "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1660273739l/11870085._SX318_.jpg",
+        about  : "Despite the tumor-shrinking medical miracle that has bought her a few years, Hazel has never been anything but terminal, her final chapter inscribed upon diagnosis. But when a gorgeous plot twist named Augustus Waters suddenly appears at Cancer Kid Support Group, Hazel's story is about to be completely rewritten.Insightful, bold, irreverent, and raw, The Fault in Our Stars is award-winning author John Green's most ambitious and heartbreaking work yet, brilliantly exploring the funny, thrilling, and tragic business of being alive and in love."
+
+    },
+    {   id : 4,
+        title  : "It Ends with Us",
+        content : "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1470427482l/27362503._SY475_.jpg",
+        about  : "Sometimes it is the one who loves you who hurts you the most.Lily hasn’t always had it easy, but that’s never stopped her from working hard for the life she wants. She’s come a long way from the small town in Maine where she grew up — she graduated from college, moved to Boston, and started her own business. So when she feels a spark with a gorgeous neurosurgeon named Ryle Kincaid, everything in Lily’s life suddenly seems almost too good to be true."
+
+    },
+    {   id : 5,
+        title  : "They Both Die at the End",
+        content : "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1494333138l/33385229._SY475_.jpg",
+        about  : "Adam Silvera reminds us that there’s no life without death and no love without loss in this devastating yet uplifting story about two people whose lives change over the course of one unforgettable day. On September 5, a little after midnight, Death-Cast calls Mateo Torrez and Rufus Emeterio to give them some bad news: They’re going to die today.Mateo and Rufus are total strangers, but, for different reasons, they’re both looking to make a new friend on their End Day. The good news: There’s an app for that. It’s called the Last Friend, and through it, Rufus and Mateo are about to meet up for one last great adventure—to live a lifetime in a single day."
+
+    },
+    {   id : 6,
+        title  : "The Assassin's Blade",
+        content : "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1384362444l/18243700.jpg",
+
+        about  : "Celaena Sardothien is Adarlan's most feared assassin. As part of the Assassin's Guild, her allegiance is to her master, Arobynn Hamel, yet Celaena listens to no one and trusts only her fellow killer-for-hire, Sam. In these action-packed novellas - together in one edition for the first time - Celaena embarks on five daring missions. They take her from remote islands to hostile deserts, where she fights to liberate slaves and seeks to avenge the tyrannous. But she is acting against Arobynn's orders and could suffer an unimaginable punishment for such treachery. Will Celaena ever be truly free? Explore the dark underworld of this kick-ass heroine to find out."
+
+    },
+    {   id : 7,
+        title  : "Verity",
+        content : "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1634158558l/59344312._SY475_.jpg",
+        about  : "Lowen Ashleigh is a struggling writer on the brink of financial ruin when she accepts the job offer of a lifetime. Jeremy Crawford, husband of bestselling author Verity Crawford, has hired Lowen to complete the remaining books in a successful series his injured wife is unable to finish. Lowen arrives at the Crawford home, ready to sort through years of Verity's notes and outlines, hoping to find enough material to get her started. What Lowen doesn't expect to uncover in the chaotic office is an unfinished autobiography Verity never intended for anyone to read. Page after page of bone-chilling admissions, including Verity's recollection of what really happened the day her daughter died."
+
+    },
+    {   id : 8,
+        title  : "The Book Thief",
+        content : "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1522157426l/19063._SY475_.jpg",
+        about  : "It is 1939. Nazi Germany. The country is holding its breath. Death has never been busier, and will be busier still.By her brother's graveside, Liesel's life is changed when she picks up a single object, partially hidden in the snow. It is The Gravedigger's Handbook, left behind there by accident, and it is her first act of book thievery. So begins a love affair with books and words, as Liesel, with the help of her accordian-playing foster father, learns to read. Soon she is stealing books from Nazi book-burnings, the mayor's wife's library, wherever there are books to be found. But these are dangerous times. When Liesel's foster family hides a Jew in their basement, Liesel's world is both opened up, and closed down."
+
+    },
+    
+]
+export default data;
